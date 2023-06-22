ef6ff6e9d07ac7bc480ebdf353d0fc2719c4c731
Victor <victordevfront@gmail.com>
2023-06-22 13:35:10 -0300

    integração do Springboot com Keycloak_21

---
+++ SpringBoot/keycloak_21/src/test/java/com/integration/keycloak/KeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
